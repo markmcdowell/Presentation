ca97e48b6ab9bfa71ea54fb4dd223c2a70833dc3
Mark <mark.mcdowell@me.com>
2016-10-19 22:11:03 +0100

    Correcting arguments

--- Presentation.Reactive/RetryObservableExtensions.cs
+++ Presentation.Reactive/RetryObservableExtensions.cs
@@ -18,7 +18,7 @@ namespace Presentation.Reactive
         public static IObservable<T> RetryAfter<T>(this IObservable<T> source, TimeSpan period, int count, IScheduler scheduler = null)
         {
             if (source == null)
-                throw new ArgumentNullException("source");
+                throw new ArgumentNullException(nameof(source));
 
             return RepeatObservableExtensions.RepeatFinite(source, i => period, count, scheduler).Catch();
         }
@@ -26,7 +26,7 @@ namespace Presentation.Reactive
         public static IObservable<T> RetryAfter<T>(this IObservable<T> source, TimeSpan period, int count, Action<Exception> onError, IScheduler scheduler = null)
         {
             if (source == null)
-                throw new ArgumentNullException("source");
+                throw new ArgumentNullException(nameof(source));
 
             return RepeatObservableExtensions.RepeatFinite(source, i => period, count, scheduler).Do(_ => { }, onError).Catch();
         }
