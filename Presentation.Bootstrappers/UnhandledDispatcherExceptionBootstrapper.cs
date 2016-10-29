7d3da1766966b5f45fac59b37d450e15632e96f0
Mark <mark.mcdowell@me.com>
2016-10-29 20:00:11 +0100

    adding unhandled dispatcher exception bootstrapper

---
+++ Presentation.Bootstrappers/UnhandledDispatcherExceptionBootstrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using Presentation.Interfaces;
+
+namespace Presentation.Bootstrappers
+{
+    public sealed class UnhandledDispatcherExceptionBootstrapper : IBootstrapper
+    {
+        private readonly IBootstrapper _bootstrapper;
+        private readonly Func<Exception, bool> _handler;
+
+        public UnhandledDispatcherExceptionBootstrapper(IBootstrapper bootstrapper, Func<Exception, bool> handler)
+        {
+            _bootstrapper = bootstrapper;
+            _handler = handler;
+        }
+
+        public void Dispose()
+        {
+            Application.Current.DispatcherUnhandledException -= CurrentOnDispatcherUnhandledException;
+
+            _bootstrapper.Dispose();
+        }
+
+        public void Initialize()
+        {
+            Application.Current.DispatcherUnhandledException += CurrentOnDispatcherUnhandledException;
+
+            _bootstrapper.Initialize();
+        }
+
+        private void CurrentOnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (_handler(e.Exception))
+                e.Handled = true;
+        }
+    }
+}
