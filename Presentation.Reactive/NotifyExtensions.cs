7b18cd645ea30409df172c9cfb99217d896327a6
Mark <mark.mcdowell@me.com>
2016-11-01 06:55:01 +0000

    Checking for null arguments

--- Presentation.Reactive/NotifyExtensions.cs
+++ Presentation.Reactive/NotifyExtensions.cs
@@ -9,6 +9,11 @@ namespace Presentation.Reactive
     {
         public static IObservable<T> Observe<T>(this T source, string propertyName) where T : INotifyPropertyChanged
         {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+
             return Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(h => source.PropertyChanged += h, h => source.PropertyChanged -= h)
                              .Where(_ => _.EventArgs.PropertyName == propertyName)
                              .Select(_ => (T)_.Sender);
@@ -16,6 +21,11 @@ namespace Presentation.Reactive
 
         public static IObservable<T> Observe<T>(this INotifyPropertyChanging source, string propertyName) where T : INotifyPropertyChanging
         {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+
             return Observable.FromEventPattern<PropertyChangingEventHandler, PropertyChangingEventArgs>(h => source.PropertyChanging += h, h => source.PropertyChanging -= h)
                              .Where(_ => _.EventArgs.PropertyName == propertyName)
                              .Select(_ => (T)_.Sender);
@@ -23,6 +33,9 @@ namespace Presentation.Reactive
 
         public static IObservable<T> Observe<T>(this T source, NotifyCollectionChangedAction action) where T : INotifyCollectionChanged
         {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
             return Observable.FromEventPattern<NotifyCollectionChangedEventHandler, NotifyCollectionChangedEventArgs>(h => source.CollectionChanged += h, h => source.CollectionChanged -= h)
                              .Where(_ => _.EventArgs.Action == action)
                              .Select(_ => (T)_.Sender);
