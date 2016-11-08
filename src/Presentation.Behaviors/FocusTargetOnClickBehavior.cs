3b3cdbcc1a91de4cfb709debce3411d44e9ce2b2
Mark <mark.mcdowell@me.com>
2016-11-08 22:59:28 +0000

    adding FocusTargetOnClickBehavior behavior

---
+++ src/Presentation.Behaviors/FocusTargetOnClickBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+
+namespace Presentation.Behaviors
+{
+    /// <summary>
+    /// Attached behavior to focus the target <see cref="UIElement"/> on <see cref="ButtonBase.Click"/>.
+    /// </summary>
+    public sealed class FocusTargetOnClickBehavior : Behavior<ButtonBase>
+    {
+        public static readonly DependencyProperty TargetProperty = DependencyProperty.Register("Target", typeof(UIElement), typeof(FocusTargetOnClickBehavior), new PropertyMetadata(default(UIElement)));
+
+        /// <summary>
+        /// Makes the specified Target the focus on click.
+        /// </summary>
+        public UIElement Target
+        {
+            get { return (UIElement)GetValue(TargetProperty); }
+            set { SetValue(TargetProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.Click += OnClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Click -= OnClick;
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Target.IsKeyboardFocusWithin)
+                return;
+
+            Target.Focus();
+        }
+    }
+}
