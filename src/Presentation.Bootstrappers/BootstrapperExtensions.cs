625a3961436bc61db738f055ad22d23d09dd9826
Mark <mark.mcdowell@me.com>
2016-11-12 23:19:43 +0000

    adding bootstrapper extensions

---
+++ src/Presentation.Bootstrappers/BootstrapperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Composition.Hosting;
+using System.Windows;
+using Presentation.Interfaces;
+
+namespace Presentation.Bootstrappers
+{
+    /// <summary>
+    /// Extensions on <see cref="IBootstrapper"/>
+    /// </summary>
+    public static class BootstrapperExtensions
+    {
+        public static IBootstrapper InitalizeModules(this IBootstrapper bootstrapper, ExportProvider exportProvider)
+        {
+            return new ModuleInitializingBootstrapper(bootstrapper, exportProvider);
+        }
+
+        public static IBootstrapper CatchTaskExceptions(this IBootstrapper bootstrapper, Func<AggregateException, bool> handler)
+        {
+            return new UnobservedTaskExceptionBootstrapper(bootstrapper, handler);
+        }
+
+        public static IBootstrapper CatchAppDomainExceptions(this IBootstrapper bootstrapper, AppDomain appDomain, Action<Exception, bool> handler)
+        {
+            return new UnhandledAppDomainExceptionBootstrapper(bootstrapper, appDomain, handler);
+        }
+
+        public static IBootstrapper CatchDispatcherExceptions(this IBootstrapper bootstrapper, Application application, Func<Exception, bool> handler)
+        {
+            return new UnhandledDispatcherExceptionBootstrapper(bootstrapper, application, handler);
+        }
+    }
+}
